f76b114a778ac1fdbeb148a7076ffd6db2e35de0
Luis Sergio Oliveira <euluis@tigris.org>
2008-06-24 00:17:18 +0000

    Issue #5175: removed preprocessed files only limitation.
    git-svn-id: http://argouml-cpp.tigris.org/svn/argouml-cpp/trunk@233 2f534547-b31d-0410-8985-b8bd6d4fc62e

--- src/org/argouml/language/cpp/reveng/CppImport.java
+++ src/org/argouml/language/cpp/reveng/CppImport.java
@@ -1,5 +1,5 @@
 // $Id$
-// Copyright (c) 2005-2007 The Regents of the University of California. All
+// Copyright (c) 2005-2008 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -166,8 +166,6 @@ public class CppImport implements ImportInterface {
                 .getProperty("line.separator")
             + "    * ";
         String warnMsg = "Its known limits are: "
-            + lineSepAndListIndent
-            + "preprocessed files only, i.e., works on full translation units;"
             + lineSepAndListIndent
             + "very few C++ constructs are supported, e.g., enums, unions, "
             + "templates, etc, aren't;"
